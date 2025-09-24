49fe7b409d1a87f70c722ef7f764dda30ba82e35
Wuuu <sucbooo@gmail.com>
2025-09-25 01:21:52 +0800

    Update xhs_filter.js

--- xhs_filter.js
+++ xhs_filter.js
@@ -1,19 +1,19 @@
 /*
- 小红书首页过滤短视频，只保留纯图文笔记
+ 小红书首页过滤短视频，只保留图文
 */
+
 let body = $response.body;
 
 try {
   let obj = JSON.parse(body);
 
   if (obj && obj.data && Array.isArray(obj.data)) {
+    // 过滤，只保留图文 note
+    console.log(obj.data.length())
     obj.data = obj.data.filter(item => {
-      // 保留条件：是note && 有images_list && note_attributes 不含 "video"
-      const isNote = item.model_type === "note";
-      const hasImages = item.images_list && item.images_list.length > 0;
-      const isVideoNote = item.note_attributes && item.note_attributes.includes("video");
-      return isNote && hasImages && !isVideoNote;
+      return item.model_type !== "video" ;
     });
+    console.log(obj.data.length())
   }
 
   body = JSON.stringify(obj);
