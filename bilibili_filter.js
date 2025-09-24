0b0f5d56f39c087cb6475ae9e98ec782adcd125f
Wuuu <sucbooo@gmail.com>
2025-09-25 02:12:34 +0800

    Create bilibili_filter.js

---
+++ bilibili_filter.js
@@ -0,0 +1,22 @@
+/*
+ 小红书首页过滤短视频，只保留图文
+*/
+
+let body = $response.body;
+
+try {
+  let obj = JSON.parse(body);
+
+  if (obj && obj.data && Array.isArray(obj.data)) {
+    // 过滤，只保留图文 note
+    console.log(obj.data.items.length)
+    obj.data.items = obj.data.items.filter(item => !item.hasOwnProperty('rcmd_reason_style'))
+    console.log(obj.data.items.length)
+  }
+// 1111
+  body = JSON.stringify(obj);
+} catch (e) {
+  console.log("xhs_filter parse error: " + e);
+}
+
+$done({ body });
