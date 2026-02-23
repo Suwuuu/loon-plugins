2433429960f46c8b9a3933f55a444bd96684f41b
Wuuu <sucbooo@gmail.com>
2026-02-23 23:36:59 +0800

    Update bili_no_swipe.js

--- bili_no_swipe.js
+++ bili_no_swipe.js
@@ -2,19 +2,26 @@ let body = $response.body;
 
 try {
     let obj = JSON.parse(body);
-    // 判断是否有 data 字段
+    
+    // 判断是否存在 data 字段
     if (obj && obj.data) {
-        // 清空不同接口可能返回的列表字段
-        if (obj.data.list) obj.data.list = [];
-        if (obj.data.items) obj.data.items = [];
-        if (obj.data.related) obj.data.related = [];
-        if (obj.data.awemes) obj.data.awemes = [];
+        // B站竖屏下滑通常使用 list 或 awemes 字段来返回新视频
+        if (obj.data.list) {
+            obj.data.list = [];
+        }
+        if (obj.data.awemes) {
+            obj.data.awemes = [];
+        }
+        if (obj.data.items) {
+            obj.data.items = [];
+        }
         
-        // 可选：修改提示语，让你知道脚本生效了
+        // 加上一个小提示，当你下滑时如果有 Toast 提示，会显示这段话
         if (obj.data.toast) {
-            obj.data.toast = "已开启防沉迷：看完请退出";
+            obj.data.toast = "防沉迷生效：请退出当前视频";
         }
     }
+    
     body = JSON.stringify(obj);
 } catch (e) {
     console.log("B站竖屏防沉迷脚本解析异常: " + e);
